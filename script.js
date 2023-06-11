ae0a8f8308a14ab7b99dcd68a69e48de853af4ec
mannmanjeet988 <120735535+mannmanjeet988@users.noreply.github.com>
2023-06-11 15:51:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,19 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
   //   write your code here
+	
+    let ans = false;
+    // write your code here
+    for (const x in sampleObject) {
+        if (key == x) {
+            ans = true;
+            return ans;
+        }
+    }
+    return ans;
+	//return sampleObject != null && hasOwnProperty.call(sampleObject, key);
 }
+
 
 // Do not change the code below
 const key = prompt("Enter Key.");
